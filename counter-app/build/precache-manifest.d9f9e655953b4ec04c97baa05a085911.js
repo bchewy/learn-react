87f3d8a6757f373e42d96fa2f111741364a0222f
brianchew <brian@bchewy.com>
2019-03-16 15:25:21 +0400

    new stuff into this repo

---
+++ counter-app/build/precache-manifest.d9f9e655953b4ec04c97baa05a085911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7378703bae832d4f411c",
+    "url": "/static/css/main.1c924863.chunk.css"
+  },
+  {
+    "revision": "7378703bae832d4f411c",
+    "url": "/static/js/main.7378703b.chunk.js"
+  },
+  {
+    "revision": "8fa89ccb2ae2582c3d80",
+    "url": "/static/css/1.a63a1d70.chunk.css"
+  },
+  {
+    "revision": "8fa89ccb2ae2582c3d80",
+    "url": "/static/js/1.8fa89ccb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37cc4f946c464e7264cd63bbdd541240",
+    "url": "/index.html"
+  }
+];
